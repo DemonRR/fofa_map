c8839bdc98528272361bed0cbd5cf0940a2d6c60
DemonRR <95859455+DemonRR@users.noreply.github.com>
2024-12-06 11:33:49 +0800

    更新
    更新

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
-#### 使用方法：
-usage: fofa.py [-h] [-q QUERY] [-s SIZE] [-o OUTFILE]
-![image](https://github.com/user-attachments/assets/bf53129c-ac9e-4274-b7e1-da69ad6f9825)
+**一.安装说明**
+
+1.首次使用请使用 **pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple **命令，安装必要的外部依赖包。
+
+2.在使用该工具前，请先填写用户信息[info]中的email和key，**fofa.ini**配置文件说明如下：
+
+```
+[info]
+#fofa注册时的邮箱
+email = 
+#fofa会员的key
+key = 
+
+[fields]
+#查询内容选项
+fields = host,protocol,ip,port,title,domain,country
+#fields可选项：['host', 'title', 'ip', 'domain', 'port', 'country', 'province', 'city', 'country_name', 'header',
+#              'server','protocol', 'banner', 'cert', 'isp', 'as_number', 'as_organization', 'latitude',
+#              'longitude', 'structinfo','icp', 'fid', 'cname']
+
+[size]
+#每页查询数量，默认为100条，最大支持10,000条/页
+size = 10
+
+[full]
+#默认搜索一年内的数据，指定为true即可搜索全部数据，false为一年内数据
+full = False
+
+[output]
+#默认不自动输出结果为Excel文件
+output = False
+```
+
+**二.使用方法**
+
+```
+python fofa.py -h                                                                                                                                            
+usage: fofa.py [-h] [-q QUERY] [-s SIZE] [-o OUTFILE]                                                                                                
+FOFA API查询工具                                                                                                                                                                 
+options:                                                                                      
+  -h, --help            show this help message and exit
+  
+  -q QUERY, --query QUERY   FOFA查询语句                                                                                                                                               
+  -s SIZE, --size SIZE  设置查询输出条数                                                                                                                                           
+  -o OUTFILE, --outfile OUTFILE 保存查询结果文件名
+```
+
+![Snipaste_2024-12-06_11-24-27](E:\主文件夹\桌面\assets\Snipaste_2024-12-06_11-24-27.png)
+
--- fofa.py
+++ fofa.py
@@ -4,6 +4,7 @@ import requests
 import argparse
 import configparser
 import pandas as pd
+from wcwidth import wcswidth    # 打印表格自动调整宽度，很重要
 from tabulate import tabulate
 from colorama import Fore, Style
 
@@ -11,80 +12,99 @@ from colorama import Fore, Style
 # 检测账号状态
 def check_status(key):
     url = f'https://fofa.info/api/v1/info/my?key={key}'
-    response = requests.get(url).json()
-    if response.get('error'):
+    try:
+        response = requests.get(url).json()
+        if response.get('error'):
+            return False
+        else:
+            return True
+    except requests.RequestException as e:
+        print(Fore.RED + f'请求账号状态信息时出现异常: {e}' + Style.RESET_ALL)
         return False
-    else:
-        return True
 
 # 查询用户信息
 def my_info(key):
     url = f'https://fofa.info/api/v1/info/my?key={key}'
-    response = requests.get(url).json()
-    print(Fore.CYAN + "======账号信息=======" + Style.RESET_ALL)
-    print(f"[+] 用户名：{response.get('username')}")
-    print(f"[+] F币剩余数量：{response.get('fcoin')}")
-    print(f"[+] VIP状态：{response.get('isvip')}")
-    print(f"[+] VIP等级：{response.get('vip_level')}")
+    try:
+        response = requests.get(url).json()
+        print(Fore.CYAN + "======账号信息=======" + Style.RESET_ALL)
+        print(f"[+] 用户名：{response.get('username')}")
+        print(f"[+] F币剩余数量：{response.get('fcoin')}")
+        print(f"[+] VIP状态：{response.get('isvip')}")
+        print(f"[+] VIP等级：{response.get('vip_level')}")
+    except requests.RequestException as e:
+        print(Fore.RED + f'获取用户信息时出现异常: {e}' + Style.RESET_ALL)
 
 # FOFA查询主题
 def get_search(q, size, email, key, fields):
     qb = base64.b64encode(q.encode('utf-8')).decode('utf-8')
     url = f'https://fofa.info/api/v1/search/all?email={email}&key={key}&size={size}&fields={fields}&qbase64={qb}'
-    response = requests.get(url).json()
-    result_data = response.get('results')
-    if result_data:
-        headers_list = list(fields.split(','))
-        table = tabulate(result_data, headers=headers_list, tablefmt="psql")
-        print(Fore.CYAN + "======查询内容=======" + Style.RESET_ALL)
-        print(f"[+] 查询语句：{q}")
-        print(f"[+] 查询字段：{fields}")
-        print(f"[+] 查询条数：{size}")
-        print(Fore.CYAN + "======查询结果=======" + Style.RESET_ALL)
-        print(table)
-        return result_data, headers_list
-    else:
-        print(Fore.RED + '查询无结果!' + Style.RESET_ALL)
-        quit()
+    try:
+        response = requests.get(url).json()
+        result_data = response.get('results')
+        if result_data:
+            headers_list = list(fields.split(','))
+            table = tabulate(result_data, headers=headers_list, tablefmt="psql")
+            print(Fore.CYAN + "======查询内容=======" + Style.RESET_ALL)
+            print(f"[+] 查询语句：{q}")
+            print(f"[+] 查询字段：{fields}")
+            print(f"[+] 查询条数：{size}")
+            print(Fore.CYAN + "======查询结果=======" + Style.RESET_ALL)
+            print(table)
+            return result_data, headers_list
+        else:
+            print(Fore.RED + '查询无结果!' + Style.RESET_ALL)
+            return [], []  # 如果没有查询结果，返回空列表
+    except requests.RequestException as e:
+        print(Fore.RED + f'执行查询时出现异常: {e}' + Style.RESET_ALL)
+        return [], []  # 如果请求失败，返回空列表
 
 # 保存查询结果为Excel
 def save_excel(result_data, headers_list, filename):
-    df = pd.DataFrame(result_data, columns=headers_list)
-    filename_time = f"查询结果_{int(time.time())}.xlsx"
-    if filename:
-        df.to_excel(filename, index=False)
-        print(Fore.CYAN + "======保存文件=======" + Style.RESET_ALL)
-        print(f"[+] 文件保存成功，文件名为：{filename}")
+    if result_data:  # 只有当查询有结果时才进行保存
+        df = pd.DataFrame(result_data, columns=headers_list)
+        filename_time = f"查询结果_{int(time.time())}.xlsx"
+        if filename or output:      # 如果没有指定输出文件名，且配置中的output为True，才自动保存默认文件名的文件
+            output_filename = filename or filename_time  # 如果指定了文件名，则使用指定的，否则使用默认文件名
+            df.to_excel(output_filename, index=False)
+            print(Fore.CYAN + "======保存文件=======" + Style.RESET_ALL)
+            print(f"[+] 文件保存成功，文件名为：{output_filename}")
     else:
-        df.to_excel(filename_time, index=False)
-        print(Fore.CYAN + "======保存文件=======" + Style.RESET_ALL)
-        print(f"[+] 文件保存成功，默认文件名为：{filename_time}")
+        print(Fore.RED + '没有查询结果可保存!' + Style.RESET_ALL)
 
-if __name__ == '__main__':
+# 从配置文件加载设置
+def load_config():
     config = configparser.ConfigParser()
     config.read('fofa.ini', encoding='utf-8')
-    email = config.get('info', 'email')
-    key = config.get('info', 'key')
+    email = config.get('info', 'email', fallback=None)
+    key = config.get('info', 'key', fallback=None)
     fields = config.get('fields', 'fields')
-    default_size = config.get('size', 'size')
+    default_size = config.get('size', 'size', fallback='100')
+    full = config.get('full', 'full', fallback='true').lower() == 'true'
+    output = config.get('output', 'output', fallback='true').lower() == 'true'
+    return email, key, fields, default_size, full, output
 
+
+if __name__ == '__main__':
+    email, key, fields, default_size, full, output = load_config()    # 加载配置
+
+    if not email or not key:
+        print(Fore.RED + '配置文件缺少必要的email或key!' + Style.RESET_ALL)
+        exit(1)
+
+    # 解析命令行参数
     parser = argparse.ArgumentParser(description="FOFA API查询工具")
     parser.add_argument('-q', '--query', type=str, help='FOFA查询语句')
-    parser.add_argument('-s', '--size', help='设置查询输出条数')
-    parser.add_argument('-o', '--outfile', help='保存查询结果')
+    parser.add_argument('-s', '--size', type=int, help='设置查询输出条数', default=default_size)
+    parser.add_argument('-o', '--outfile', help='保存查询结果文件名')
     args = parser.parse_args()
-    filename = args.outfile
-    if check_status(key):
-        if args.size:
-            size = args.size
-        else:
-            size = default_size
 
+    if check_status(key):   # 判断运行方式
         if args.query:
             my_info(key)
-            result_data, headers_list = get_search(args.query, size, email, key, fields)
-            save_excel(result_data, headers_list, filename)
+            result_data, headers_list = get_search(args.query, args.size, email, key, fields)
+            save_excel(result_data, headers_list, args.outfile)
         else:
             my_info(key)
     else:
-        print(Fore.RED + 'key无效!,请检测配置文件是否填写正确!' + Style.RESET_ALL)
+        print(Fore.RED + 'key无效!,请检查配置文件是否填写正确!' + Style.RESET_ALL)
